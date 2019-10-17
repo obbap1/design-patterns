eb05c36c64f95b2878aff88c8759d0cdb0071b7d
Paschal Obba <paschalobba@gmail.com>
2019-10-18 00:20:45 +0100

    Feature: the singleton pattern

--- SingletonPattern.cpp
+++ SingletonPattern.cpp
@@ -1,19 +1,26 @@
+/**
+ * I think the intention of the singleton pattern, is to ensure we cant create instances of a particular class explicitly,
+ *  so we can just access the public methods.
+*/
 
 #include <iostream>
 
 using namespace std;
 
+//Singleton Class
 class SingleObject {
     private:
+        //Make the constructor private, so instances of the class will be forbidden.
         SingleObject(){};
 
     public:
+        //return an instance of the class
         static SingleObject* getInstance(){
             return (!my_singleton) ? 
                 my_singleton = new SingleObject :
                 my_singleton;
         };
-
+        //A public method
         void showMessage(){
             cout << "Hello World!" << endl;
         };
